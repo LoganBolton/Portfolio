bf00a983be560f5e2914366784202986ebacef97
Logan Bolton <loganbolton101@gmail.com>
2023-09-10 21:36:59 -0500

    tailwind

---
+++ portfolio/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    <div className="p-6">
+          <h2 className="text-xl font-bold text-gray-800">Card Title</h2>
+          <p className="text-gray-600 mt-2">This is some card content. Tailwind makes it easy to style and layout these elements.</p>
+          <button className="mt-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 border border-blue-700 rounded">
+            Read More
+          </button>
+        </div>
+
+    </div>
+  );
+}
+
+export default App;
