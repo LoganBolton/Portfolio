7abedf89b10436dff25479307f3949e3a4b0c3cf
Logan Bolton <loganbolton101@gmail.com>
2023-09-23 23:31:24 -0500

    Add icon

--- portfolio/src/components/Technologies.js
+++ portfolio/src/components/Technologies.js
@@ -3,41 +3,43 @@ import '../css/Technologies.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHtml5, faCss3, faJs, faReact, faGithub, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
 
+import htmlIcon from '../images/html.png';
+import cssIcon from '../images/css.png';
+import jsIcon from '../images/javascript.png';
+import dotNetIcon from '../images/aspnet.svg';
+import reactIcon from '../images/communityIcon_4g1uo0kd87c61.png';
+import csharpIcon from '../images/c-sharp-c-icon-1822x2048-wuf3ijab.png';
 const Technologies = () => {
   return (
-    <div className="tech w-full">
-      <div className='title'>
+    <div className="techList w-full">
+      <div className='techTitle'>
         <p>Technologies</p>
       </div>
       <div className='tech-list'>
-        <ul className=''>
+        <ul className='items-center'>
           <li className='technology bg-white p-4 rounded flex flex-col items-center'>
-              <FontAwesomeIcon icon={faHtml5} />
+              <img className="icon" src={htmlIcon} alt="HTML icon" />
               <span>HTML</span>
           </li>
           <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <FontAwesomeIcon icon={faCss3} />
+              <img className="icon" src={cssIcon}/>
               <span>CSS</span>
           </li>
           <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <FontAwesomeIcon icon={faJs} />
+              <img className="icon" src={jsIcon}/>
               <span>JavaScript</span>
           </li>
           <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <FontAwesomeIcon icon={faMicrosoft} />
+              <img className="icon" src={csharpIcon}/>
               <span>C#</span>
           </li>
           <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <FontAwesomeIcon icon={faMicrosoft} />
+              <img className="icon" src={dotNetIcon}/>
               <span>.Net</span>
           </li>
           <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <FontAwesomeIcon icon={faReact} />
+              <img className="icon" src={reactIcon}/>
               <span>React</span>
-          </li>
-          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <FontAwesomeIcon icon={faGithub} />
-              <span>GitHub</span>
           </li>
         </ul>
       </div>
