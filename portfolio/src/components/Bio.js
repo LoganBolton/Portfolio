abada6e90392fc56580600483e84bd79dbe84925
Logan Bolton <loganbolton101@gmail.com>
2023-09-24 22:20:25 -0500

    back to basics

--- portfolio/src/components/Bio.js
+++ portfolio/src/components/Bio.js
@@ -1,6 +1,7 @@
 // import React from 'react';
 import React, { useEffect, useState} from 'react';
 import headshot from '../images/headshot.jpeg'; 
+import dino from '../images/dino.jpg';
 
 const Bio = () => {
   useEffect(() => {
@@ -12,25 +13,30 @@ const Bio = () => {
   return (
     <div className="BioDiv flex gray-600 rounded-lg justify-center h-[95vh] items-center"  id = "fadeDiv" style={{ opacity: 0, transition: 'opacity 2s ease-in-out' }}>
 
-        {/* Headshot */}
-        <div className="bioImage w-[13rem] flex items-center">
-            <img className="rounded-3xl" src={headshot} />
-        </div>
-        {/* Blue Line */}
-        <div className="bg-blue-300 w-1 h-32 ml-[2rem] mr-[2rem]">&nbsp;</div> 
-        <div className = "title text-left w-[35rem]">
-            <div className="font-bold text-5xl mb-2">Logan Bolton</div>
-            <div className="italic text-sm mb-3">Software Engineering Student - Auburn, AL</div>
-            
+        <div className='bioCard items-center flex'>
+          {/* Headshot */}
+          <div className="bioImage w-[13rem] flex items-center">
+              <img className="rounded-3xl" src={headshot} />
+          </div>
+          {/* Blue Line */}
+          <div className="bg-blue-300 w-1 h-32 ml-[2rem] mr-[2rem]">&nbsp;</div> 
+          <div className = "title text-left w-[35rem]">
+              <div className="font-bold text-5xl mb-2">Logan Bolton</div>
+              <div className="italic text-sm mb-3">Software Engineering Student - Auburn, AL</div>
+              
 
-            <div className="text-lg">
-              Hi! My name is Logan Bolton. I am a Junior majoring in Software Engineering at Auburn University. I am currently working as a co-op student at Auburn
-              {' '}<a href="https://cws.auburn.edu/cws" target='_blank' className="text-blue-400 hover:underline">Campus Web Solutions</a>
-              . I enjoy weight lifting, listening to music and being around friends. Once I graduate in Spring of 2025, I plan to attend graduate school and
-              earn a Master's degree in Computer Science. 
-            </div>
+              <div className="text-lg">
+                Hi! My name is Logan Bolton. I am a Junior majoring in Software Engineering at Auburn University. I am currently working as a co-op student at Auburn
+                {' '}<a href="https://cws.auburn.edu/cws" target='_blank' className="text-blue-400 hover:underline">Campus Web Solutions</a>
+                . I enjoy weight lifting, listening to music and being around friends. Once I graduate in Spring of 2025, I plan to attend graduate school and
+                earn a Master's degree in Computer Science. 
+              </div>
+          </div>
         </div>
 
+        {/* <div>
+          <img className="dinoImage" src={dino} alt="chrome dinosaur"/>
+        </div> */}
       </div>
   );
 };
--- portfolio/src/components/Experience.js
+++ portfolio/src/components/Experience.js
@@ -2,8 +2,31 @@ import React from 'react';
 
 const Experience = () => {
     return (
-        <div className='techTitle font-bold text-white text-3xl mb-[1rem]'>
-            <p>Projects</p>
+        <div className="experience-container">
+            <div className='techTitle font-bold text-white text-3xl mb-[1rem]'>
+                <p>Projects</p>
+            </div>
+            {/* Project Card */}
+            <div className = "cardsDiv flex">
+                <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-10">
+                    <div className="md:flex">
+                        <div className="p-8">
+                            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Project Title</div>
+                            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Project heading</a>
+                            <p className="mt-2 text-gray-500">Project description goes here. Briefly explain your project.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-10">
+                    <div className="md:flex">
+                        <div className="p-8">
+                            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Project Title</div>
+                            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Project heading</a>
+                            <p className="mt-2 text-gray-500">Project description goes here. Briefly explain your project.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
