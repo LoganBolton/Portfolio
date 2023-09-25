1733438ce780107a0d73f3f79ecca21cdd1fb6a9
Logan Bolton <loganbolton101@gmail.com>
2023-09-24 22:31:56 -0500

    nice

--- portfolio/src/components/Experience.js
+++ portfolio/src/components/Experience.js
@@ -7,23 +7,19 @@ const Experience = () => {
                 <p>Projects</p>
             </div>
             {/* Project Card */}
-            <div className = "cardsDiv flex">
-                <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-10">
-                    <div className="md:flex">
-                        <div className="p-8">
-                            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Project Title</div>
-                            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Project heading</a>
-                            <p className="mt-2 text-gray-500">Project description goes here. Briefly explain your project.</p>
-                        </div>
+            <div className = "cardsDiv flex justify-evenly">
+                <div className="max-w-md bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl my-10">
+                    <div className="p-8">
+                        <div className="uppercase tracking-wide text-xl text-indigo-500 font-semibold">Project Title</div>
+                        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Project heading</a>
+                        <p className="mt-2 text-gray-500">Project description goes here. Briefly explain your project.</p>
                     </div>
                 </div>
-                <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-10">
-                    <div className="md:flex">
-                        <div className="p-8">
-                            <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Project Title</div>
-                            <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Project heading</a>
-                            <p className="mt-2 text-gray-500">Project description goes here. Briefly explain your project.</p>
-                        </div>
+                <div className="max-w-md bg-white rounded-xl shadow-lg overflow-hidden md:max-w-2xl my-10">
+                    <div className="p-8">
+                        <div className="uppercase tracking-wide text-xl text-indigo-500 font-semibold">Project Title</div>
+                        <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Project heading</a>
+                        <p className="mt-2 text-gray-500">Project description goes here. Briefly explain your project.</p>
                     </div>
                 </div>
             </div>
--- portfolio/src/components/Technologies.js
+++ portfolio/src/components/Technologies.js
@@ -17,36 +17,44 @@ const Technologies = () => {
         <p>Skills</p>
       </div>
       <div className='tech-list flex justify-center m-[6rem]'>
-        <ul className='flex flex-wrap justify-around items-center w-3/4'>
-          <li className='technology bg-white p-4 rounded flex flex-col items-center'>
+        <ul className='flex flex-wrap justify-evenly items-center w-3/4'>
+          <li className='technology bg-white p-4 rounded flex flex-col items-center
+          max-w-md bg-white rounded-xl shadow-lg md:max-w-2xl my-10'>
               <img className="icon" src={htmlIcon} alt="HTML icon"/>
               <span>HTML</span>
           </li>
-          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
+          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center
+          max-w-md bg-white rounded-xl shadow-lg md:max-w-2xl my-10'>
               <img className="icon" src={cssIcon} alt = "CSS Icon"/>
               <span>CSS</span>
           </li>
-          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
+          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center
+          max-w-md bg-white rounded-xl shadow-lg md:max-w-2xl my-10'>
               <img className="icon" src={jsIcon} alt = "JS Icon"/>
               <span>JavaScript</span>
           </li>
-          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <img className="icon" src={csharpIcon} alt = "C Sharp Icon"/>
-              <span>C#</span>
-          </li>
-          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <img className="icon" src={dotNetIcon} alt = "Dot Net Icon"/>
-              <span>.Net</span>
-          </li>
-          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
-              <img className="icon" src={reactIcon} alt = "React Icon"/>
-              <span>React</span>
-          </li>
-          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
+          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center
+          max-w-md bg-white rounded-xl shadow-lg md:max-w-2xl my-10'>
               <img className="icon" src={jqueryIcon} alt = "JQuery Icon"/>
               <span>JQuery</span>
           </li>
-          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center'>
+          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center
+          max-w-md bg-white rounded-xl shadow-lg md:max-w-2xl my-10'>
+              <img className="icon" src={csharpIcon} alt = "C Sharp Icon"/>
+              <span>C#</span>
+          </li>
+          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center
+          max-w-md bg-white rounded-xl shadow-lg md:max-w-2xl my-10'>
+              <img className="icon" src={dotNetIcon} alt = "Dot Net Icon"/>
+              <span>.Net</span>
+          </li>
+          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center
+          max-w-md bg-white rounded-xl shadow-lg md:max-w-2xl my-10'>
+              <img className="icon" src={reactIcon} alt = "React Icon"/>
+              <span>React</span>
+          </li>
+          <li className='technology bg-white p-4 rounded mt-2 flex flex-col items-center
+          max-w-md bg-white rounded-xl shadow-lg md:max-w-2xl my-10'>
               <img className="icon" src={pythonIcon} alt = "Python Icon"/>
               <span>Python</span>
           </li>
